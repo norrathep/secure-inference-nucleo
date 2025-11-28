1a3b6405c3393ad899be00c39d33b6aa551da1a9
Norrathep Rattanavipanon <2454392+norrathep@users.noreply.github.com>
2025-11-28 12:51:21 +0700

    Create flash_layout.h

---
+++ flash_layout.h
@@ -0,0 +1,4 @@
+//#define FLASH_S_PARTITION_SIZE          (0x2D000) /* S partition */
+//#define FLASH_NS_PARTITION_SIZE         (0x9000) /* NS partition */
+#define FLASH_S_PARTITION_SIZE          (0x23000) /* S partition */
+#define FLASH_NS_PARTITION_SIZE         (0x13000) /* NS partition */
